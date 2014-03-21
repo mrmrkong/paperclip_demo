1903179b9d2230a494832d114b425cb2b5a6da9d
Owen Kong <Owen@Owens-MacBook-Pro.local>
2014-03-21 16:50:59 -0700

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,9 +81,8 @@ PaperclipDemo::Application.configure do
   # Amazon S3 settings for Paperclip uploads
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_protocol => 'http',
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
+      :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
